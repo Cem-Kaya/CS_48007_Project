adfd79c2a4fed310ca761e07f4af3ba77995ab6d
emre <eminc2033@gmail.com>
2022-12-28 21:35:48 +0300

    sound collect and sended added

--- pi.py
+++ pi.py
@@ -9,6 +9,8 @@ from picamera2 import Picamera2, Preview
 import yaml
 import matplotlib.image as mpimg
 from PIL import Image
+import pyaudio
+import wave
 ip = "172.22.5.242"
 picam2 = Picamera2()
 
@@ -115,9 +117,50 @@ if __name__ == '__main__':
                             print(" is it false",GPIO.input(sound))
 
                             # infinite loop
-                            while GPIO.input(sound)==False:
+                            while GPIO.input(sound)==0:
                                 print("in second while")
                                 time.sleep(1)
+                            form_1 = pyaudio.paInt16 # 16-bit resolution
+                            chans = 1 # 1 channel
+                            samp_rate = 44100 # 44.1kHz sampling rate
+                            chunk = 4096 # 2^12 samples for buffer
+                            record_secs = 3 # seconds to record
+                            dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+                            wav_output_filename = 'test1.wav' # name of .wav file
+
+                            audio = pyaudio.PyAudio() # create pyaudio instantiation
+
+                            # create pyaudio stream
+                            stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                                                input_device_index = dev_index,input = True, \
+                                                frames_per_buffer=chunk)
+                            print("recording")
+                            frames = []
+
+                            # loop through stream and append audio chunks to frame array
+                            for ii in range(0,int((samp_rate/chunk)*record_secs)):
+                                data = stream.read(chunk)
+                                frames.append(data)
+
+                            print("finished recording")
+
+                            # stop the stream, close it, and terminate the pyaudio instantiation
+                            stream.stop_stream()
+                            stream.close()
+                            audio.terminate()
+
+                            # save the audio frames as .wav file
+                            wavefile = wave.open(wav_output_filename,'wb')
+                            wavefile.setnchannels(chans)
+                            wavefile.setsampwidth(audio.get_sample_size(form_1))
+                            wavefile.setframerate(samp_rate)
+                            wavefile.writeframes(b''.join(frames))
+                            wavefile.close()
+                            try:    
+                                print( re.post("http://"+ip+":5000/end_point_3", files={'sound': open('sound.wav', 'rb')}) ) 
+                                time.sleep(2) 
+                            except Exception as e:
+                                print("this expect is !!!!!! ",e)
                     except Exception as e:
                         print("expection is ..... ", e)
                             
